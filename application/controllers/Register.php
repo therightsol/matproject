89c08079f90946bdb3970d7cb8332542b458a9b4
abdulshakoor3210 <abdulshakoornawaz@gmail.com>
2016-06-07 22:49:17 +0500

    form file main first name aur username man changing ki hai abi bus
    sir nay jo aj krwaya ja us main sirf first name aur username main jo
    changing ki thi wo add kar di gai hai baki form main changing rehti hai

--- application/controllers/Register.php
+++ application/controllers/Register.php
@@ -11,7 +11,49 @@ class Register extends CI_Controller {
 
     public function index()
     {
-        $this->load->view('Register');
+
+        $data = array();
+
+        if(filter_input_array(INPUT_POST)){
+
+
+            $this->load->library('form_validation');
+
+            $config = array (
+                array(
+                    'field'     =>  'fname',
+                    'label'     =>  'First Name',
+                    'rules'     =>  'required'
+                ),
+
+                array(
+                    'field'     =>  'un',
+                    'label'     =>  'User Name',
+                    'rules'     =>  'required|alpha_numeric'
+                ),
+            );
+
+            $this->form_validation->set_rules($config);
+
+            if(! $this->form_validation->run() == FALSE){
+                // When Success
+            }else {
+                // when fails
+
+                $data['validation_errors'] = validation_errors();
+
+                $this->load->view('register', $data);
+
+                //echo validation_errors();
+            }
+
+
+
+
+        }else{
+            $this->load->view('Register', $data);
+
+        }
 
     }
 }
--- application/views/Register.php
+++ application/views/Register.php
@@ -26,104 +26,108 @@
     </div>
     <!-- End Registeration Banner -->
     <!--  Register Form -->
-    <div class="container-fluid">
-    <div class="row">
-        <div class="col-sm-3  col-sm-offset-2 noteregister">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-sm-3  col-sm-offset-2 noteregister">
 
 
 
-                <img src="<?php echo $root."assets/";?>images/mat.jpg" alt="" />
-                <p class="linote">High quality profiles and pictures.</p>
-            <p class="linote"> Ethical standards to ensure safety.</p>
-            <p class="linote">24/7 technical support for all members.</p>
-            <p class="linote"> 100% anonymity for ultimate security.</p>
-            <p class="linote">Success stories from around the world.</p>
-            <p class="linote">High quality profiles and pictures.</p>
-            <p class="linote">Find Your Match.</p>
+                    <img src="<?php echo $root."assets/";?>images/mat.jpg" alt="" />
+                    <p class="linote">High quality profiles and pictures.</p>
+                    <p class="linote"> Ethical standards to ensure safety.</p>
+                    <p class="linote">24/7 technical support for all members.</p>
+                    <p class="linote"> 100% anonymity for ultimate security.</p>
+                    <p class="linote">Success stories from around the world.</p>
+                    <p class="linote">High quality profiles and pictures.</p>
+                    <p class="linote">Find Your Match.</p>
 
 
 
 
 
 
-        </div>
+                </div>
 
-        <div class="col-sm-offset-1 col-xs-10 col-sm-6 col-md-6 form_sign">
+                <div class="col-sm-offset-1 col-xs-10 col-sm-6 col-md-6 form_sign">
 
 
-            <div class="form_register">
 
-                <form class="form-horizontal" style="margin-top:20px;" action="#" method="post">
-                    <div class="form-group">
-                        <label for="fname" class="lable_form col-sm-3 control-label">First Name</label>
-                        <div class="col-xs-10 col-sm-7 ">
-                            <input type="name" class="form-control" id="fname" placeholder="First Name">
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label for="lname" class="lable_form col-sm-3 control-label">Last Name</label>
-                        <div class="col-xs-10 col-sm-7 ">
-                            <input type="name" class="form-control" id="lname" placeholder="Last Name">
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <label for="Username" class="lable_form col-sm-3 control-label">User Name</label>
-                        <div class="col-xs-10 col-sm-7 ">
-                            <input type="name" class="form-control" id="Username" placeholder="UserName">
-                        </div>
+                    <div class="alert alert-danger">
+                        <?php echo $validation_errors; ?>
                     </div>
 
 
-
-
-                            <div class="form-group">
-                                <label for="Password" class="lable_form col-sm-offset-1 col-sm-2 control-label">Password</label>
-                                <div class="col-sm-7">
-                                    <input type="password" class="form-control" id="Password" placeholder="Password">
-                                </div>
-
+                    <form class="form-horizontal" style="margin-top:20px;" action="<?php echo $root; ?>register" method="post">
+                        <div class="form-group">
+                            <label for="fname" class="lable_form col-sm-3 control-label">First Name</label>
+                            <div class="col-xs-10 col-sm-7 ">
+                                <input type="text" name="fname" class="form-control" id="fname" placeholder="First Name">
                             </div>
-                    <div class="form-group">
-                    <label for="cPassword" class="lable_form col-sm-3 control-label">Confirm Passowrd </label>
-                    <div class="col-sm-7">
-                        <input type="password" class="form-control" id="cPassword" placeholder="Confirm Password">
-                    </div>
-                    </div>
-
-
-                    <div class="form-group">
-                        <label for="email" class="lable_form col-sm-3 control-label">Email</label>
-                        <div class="col-xs-10 col-sm-7">
-                            <input type="email" class="lable_form form-control" id="email" placeholder="Email">
                         </div>
-                    </div>
-
-
-
-                    <div class="form-group">
-                        <label for="92" class="lable_form col-sm-3 control-label">Phone No</label>
-
-
-                        <div class="col-xs-8 col-sm-7">
-                            <input type="text" class="form-control" id="92" placeholder="6214643">
+                        <div class="form-group">
+                            <label for="lname" class="lable_form col-sm-3 control-label">Last Name</label>
+                            <div class="col-xs-10 col-sm-7 ">
+                                <input  type="text" name="lname"  class="form-control" id="lname" placeholder="Last Name">
+                            </div>
                         </div>
 
-                    </div>
-
-
-
-
-                    <div class="form-group">
-                        <div class="col-sm-offset-4 col-xs-10 col-sm-4">
-                            <button type="submit" class="btn-block btn-primary">Registeration</button>
+                        <div class="form-group">
+                            <label for="Username" class="lable_form col-sm-3 control-label">User Name</label>
+                            <div class="col-xs-10 col-sm-7 ">
+                                <input  type="text" name="un"  class="form-control" id="Username" placeholder="UserName">
+                            </div>
                         </div>
-                    </div>
-                </form>
+
+
+
+
+                        <div class="form-group">
+                            <label for="Password" class="lable_form col-sm-offset-1 col-sm-2 control-label">Password</label>
+                            <div class="col-sm-7">
+                                <input type="password" name="pass"  class="form-control" id="Password" placeholder="Password">
+                            </div>
+
+                        </div>
+                        <div class="form-group">
+                            <label for="cPassword" class="lable_form col-sm-3 control-label">Confirm Passowrd </label>
+                            <div class="col-sm-7">
+                                <input type="password"  name="conpass"   class="form-control" id="cPassword" placeholder="Confirm Password">
+                            </div>
+                        </div>
+
+
+                        <div class="form-group">
+                            <label for="email" class="lable_form col-sm-3 control-label">Email</label>
+                            <div class="col-xs-10 col-sm-7">
+                                <input type="email"  name="email"   class="lable_form form-control" id="email" placeholder="Email">
+                            </div>
+                        </div>
+
+
+
+                        <div class="form-group">
+                            <label for="phonenumber" class="lable_form col-sm-3 control-label">Phone No</label>
+
+
+                            <div class="col-xs-8 col-sm-7">
+                                <input type="text" name="phone"  class="form-control" id="phonenumber" placeholder="+923001234567">
+                            </div>
+
+                        </div>
+
+
+
+
+                        <div class="form-group">
+                            <div class="col-sm-offset-4 col-xs-10 col-sm-4">
+                                <button type="submit" class="btn-block btn-primary">Registeration</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
-</div>
 
     <!--END  Register Form -->
     <!-- Start Footer Section -->
