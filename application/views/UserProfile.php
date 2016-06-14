fcf31935087d23c6fe25bb0b8ec511b2e2ac9478
Furqanafzal <furqan_afzal60@yahoo.com>
2016-06-14 13:53:15 +0500

    User Profle
    update in class

--- application/views/UserProfile.php
+++ application/views/UserProfile.php
@@ -33,7 +33,7 @@
 
 
                                 <div class="col-xs-12 col-sm-8  col-md-6 ">
-                                    <label class="control-label lable_user" for="dob">Date Of Birth<span class="lable_steric"> *</span></label>
+
                                     <input type="text" name="dob"  id="dob" placeholder="01/12/1990" class="form-control inpute_profile"/>
                                     <?php if($_POST):
                                         echo form_error('dob', '<strong class="error">', '</strong>');
@@ -41,7 +41,7 @@
                                     ?>
                                 </div>
                                 <div class="col-xs-12 col-sm-8  col-md-6 ">
-                                    <label class="control-label lable_user" for="rlgn" >Religion<span class="lable_steric"> *</span></label>
+
                                     <input type="text" name="religion"  id="rlgn" placeholder="Suni, Shia" class="form-control inpute_profile"/>
                                     <?php if($_POST):
                                         echo form_error('religion', '<strong class="red">', '</strong>');
@@ -51,7 +51,7 @@
 
 
                                     <div class="col-xs-12 col-sm-8 col-md-6 ">
-                                        <label class="control-label lable_user" for="inti">Interested In<span class="lable_steric"> *</span></label>
+
                                         <input type="text" name="intdi"  id="inti" placeholder="Interested In" class="form-control inpute_profile"/>
                                         <?php if($_POST):
                                             echo form_error('intdi', '<strong class="error">', '</strong>');
@@ -59,7 +59,7 @@
                                         ?>
                                     </div>
                      <div class="col-xs-12 col-sm-8 col-md-6 ">
-                         <label class="control-label lable_user" for="sect" >Sect<span class="lable_steric"> *</span></label>
+
                          <input type="text" name="sect" id="sect" placeholder="Sect" class="form-control inpute_profile"/>
                          <?php if($_POST):
                              echo form_error('sect', '<strong class="red">', '</strong>');
@@ -69,7 +69,7 @@
 
 
                                     <div class="col-xs-12 col-sm-8 col-md-6 ">
-                                        <label class="control-label lable_user" for="lng" >Languages<span class="lable_steric"> *</span></label>
+
                                         <input type="text" name="lng"  id="lng" placeholder="Languages" class="form-control inpute_profile"/>
                                         <?php if($_POST):
                                             echo form_error('lng', '<strong class="red">', '</strong>');
@@ -77,7 +77,7 @@
                                         ?>
                                     </div>
                                     <div class="col-xs-12 col-sm-8  col-md-6 ">
-                                        <label class="control-label lable_user" for="mtt" >Mother Tongue<span class="lable_steric"> *</span></label>
+
                                         <input type="text" name="mothertongue"  id="mtt" placeholder="Mother Tongue" class="form-control inpute_profile"/>
                                         <?php if($_POST):
                                             echo form_error('mothertongue', '<strong class="red">', '</strong>');
@@ -90,7 +90,7 @@
 
 
                                     <div class="col-xs-12 col-sm-8 col-md-6 ">
-                                        <label class="control-label lable_user" for="lcountry" >LivingIn Country <span class="lable_steric"> *</span></label>
+
                                         <input type="text" name="lcountry"  id="lcountry" placeholder="Living Country" class="form-control inpute_profile"/>
                                         <?php if($_POST):
                                             echo form_error('lcountry', '<strong class="red">', '</strong>');
@@ -98,7 +98,7 @@
                                         ?>
                                     </div>
                                     <div class="col-xs-12 col-sm-8  col-md-6">
-                                        <label class="control-label lable_user" for="lcity" >LivingIn City<span class="lable_steric"> *</span></label>
+
                                         <input type="text"name="lcity"  id="lcity" placeholder="LivingIn City" class="form-control inpute_profile"/>
                                         <?php if($_POST):
                                             echo form_error('lcity', '<strong class="red">', '</strong>');
@@ -109,7 +109,7 @@
 
 
                                     <div class="col-xs-12 col-sm-8 col-md-6 ">
-                                        <label class="control-label lable_user" for="bcountry" >Base Country<span class="lable_steric"> *</span></label>
+
                                         <input type="text" name="bcountry"  id="bcountry" placeholder="Base Country" class="form-control inpute_profile"/>
                                         <?php if($_POST):
                                             echo form_error('bcountry', '<strong class="red">', '</strong>');
@@ -117,7 +117,7 @@
                                         ?>
                                     </div>
                                     <div class="col-xs-12 col-sm-8  col-md-6 ">
-                                        <label class="control-label  lable_user" for="bcity" >Base City<span class="lable_steric"> *</span></label>
+
                                         <input type="text" name="bcity"  id="bcity" placeholder="Base City" class="form-control inpute_profile"/>
                                         <?php if($_POST):
                                             echo form_error('bcity', '<strong class="red">', '</strong>');
@@ -127,26 +127,27 @@
 
 
                                     <div class="col-xs-12 col-sm-8  col-md-6 ">
-                                        <label class="control-label lable_user" for="profession" >Profession<span class="lable_steric"> *</span></label>
+
                                         <input type="text" name="profession"  id="profession" placeholder="Profession" class="form-control inpute_profile"/>
                                         <?php if($_POST):
                                             echo form_error('profession', '<strong class="red">', '</strong>');
                                         endif;
                                         ?>
                                     </div>
+                     <div class="col-xs-12 col-sm-8  col-md-6 ">
+
+                         <Input class="form-control inpute_profile" type="text" name="interests"  rows="2" id="interest" placeholder="Interests">
+                         <?php if($_POST):
+                             echo form_error('profession', '<strong class="red">', '</strong>');
+                         endif;
+                         ?>
+                     </div>
 
 
-                                <div class="form-group col-xs-12 col-sm-8  col-md-6">
-                                    <label class="control-label lable_user" for="interest" >Interests<span class="lable_steric"> *</span></label>
-                                    <Input class="form-control inpute_profile" name="interests"  rows="2" id="interest" placeholder="Interests">
-                                    <?php if($_POST):
-                                        echo form_error('interests', '<strong class="red">', '</strong>');
-                                    endif;
-                                    ?>
-                                </div>
+                              
                                 <div class="form-group col-xs-12 col-sm-8  col-md-12">
-                                    <label class="control-label lable_user" for="aboutme" >About Me<span class="lable_steric"> *</span></label>
-                                    <input class="form-control inpute_profile" name="aboutme" rows="2" id="aboutme"  placeholder="About Me">
+
+                                    <input class="form-control inpute_profile" type="text" name="aboutme" rows="2" id="aboutme"  placeholder="About Me">
                                     <?php if($_POST):
                                         echo form_error('aboutme', '<strong class="red">', '</strong>');
                                     endif;
