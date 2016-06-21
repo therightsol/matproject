d9734d698d662b6b88fe79ece2dcb32064c5f908
abdullahchaudary <chaudary421@gmail.com>
2016-06-21 12:57:26 +0500

    issue reolved with verify page

--- application/views/verify.php
+++ application/views/verify.php
@@ -6,12 +6,21 @@
 
 <?php include 'includes/navigation.inc';?>
     <!-- Top content -->
-    <div id="container">
+<div class="top-content">
+    <div class="inner-bg">
+        <div id="container">
         <?php 
         if(!$email_alredy_verified): 
-            echo"<h1>Congratulations!</h1><br><h2>Your email has been verified. Now you can login by clicking the link below.</h2><br><h2> . $url </h2>"
-            ; endif; ?>
-        <?php if($email_alredy_verified){ echo"<h1>Sorry!</h1><br><h2>Your token has been expired!</h2>";} ?>
+            echo"<h1>Congratulations!</h1><br><h2>Your email has been verified. Now you can login by clicking the link below.</h2><br><h2> <a href='#'>Login!</a></h2>";
+            endif;
+        ?>
+        <?php
+        if($email_alredy_verified):
+            echo"<h1>Sorry!</h1><br><h2>Your token has been expired!</h2>";
+            endif;
+        ?>
+        </div>
     </div>
+</div>
 
 
